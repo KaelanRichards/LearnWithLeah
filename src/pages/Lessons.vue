3061340e8a83da275c5e3e56e6f4ca723b4d1027
kadokaelan@gmail.com <kadokaelan@gmail.com>
2019-10-15 00:20:10 -0400

    First commmit

---
+++ src/pages/Lessons.vue
@@ -0,0 +1,9 @@
+<template>
+    <layout>
+        <div class="container-inner mx-auto py-16">
+            <div class="bg-white rounded-lg p-6">
+                Hello
+            </div>
+        </div>
+    </layout>
+</template>
